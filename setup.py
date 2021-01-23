d14350a7f34d30f7da2142d69006660741e5c2ab
Janne Spijkervet <janne.spijkervet@gmail.com>
2021-01-24 00:11:32 +0100

    updated setup.py

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/spijkervet/SimCLR'
 EMAIL = 'janne.spijkervet@gmail.com'
 AUTHOR = 'Janne Spijkervet'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '1.0.0'
+VERSION = '1.0.1'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
