eb1419816f9abad999efc68f4bbd99d6bd238d99
Jonas <jonas.schweisthal@campus.lmu.de>
2021-10-25 15:17:07 +0200

    added loss to modules

--- simclr/modules/__init__.py
+++ simclr/modules/__init__.py
@@ -1,4 +1,5 @@
 from .nt_xent import NT_Xent
+from .onnpu_plus_ntxent import  PU_plus_NTXent
 from .logistic_regression import LogisticRegression
 from .lars import LARS
 from .resnet import get_resnet
